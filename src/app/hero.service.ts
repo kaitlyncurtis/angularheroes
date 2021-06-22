f4178a78ca9cd31102198a74eb8b4ba4ed150945
Kaitlyn Curtis <kaitlyn.curtis@pressganey.com>
2021-06-22 16:23:50 -0400

    Correct method naming error

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -43,7 +43,7 @@ export class HeroService {
   }
 
   /** GET hero by id. Will 404 if id not found */
-  getHerwithout404(id: number): Observable<Hero> {
+  getHeroWith404(id: number): Observable<Hero> {
     const url = `${this.heroesUrl}/${id}`;
     return this.http.get<Hero>(url).pipe(
       tap(_ => this.log(`fetched hero id=${id}`)),
